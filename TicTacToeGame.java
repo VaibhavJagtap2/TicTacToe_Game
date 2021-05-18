893f704c40df07b019ca73ea2edbdf1133b67a15
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-18 09:15:07 +0530

    create Board and passing the array

---
+++ TicTacToeGame.java
@@ -0,0 +1,17 @@
+package com.tictactoegame;
+
+public class TicTacToeGame {
+	
+	public static char[] CreateBoard() {
+		char[] board = new char[10];
+		for( int i=1;i<10;i++) {
+			board[i]=' ';
+		}
+		return board;
+}
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO THE TIC_TAC_TOE_GAME");
+		char[] board=CreateBoard();
+}
+}	
+
